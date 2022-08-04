7f8b000f2174554818ffc14d72b0a9ff773e3491
bakefayat <bakefayat98@gmail.com>
2022-08-04 11:23:24 +0430

    Create recepie tag implemented

--- app/recepies/views.py
+++ app/recepies/views.py
@@ -5,7 +5,7 @@ from rest_framework.permissions import IsAuthenticated
 from core.models import Tags
 from .serializers import TagsSerializer
 
-class TagsViewSet(viewsets.GenericViewSet, mixins.ListModelMixin):
+class TagsViewSet(viewsets.GenericViewSet, mixins.ListModelMixin, mixins.CreateModelMixin,):
     authentication_classes = (TokenAuthentication, )
     permission_classes = (IsAuthenticated, )
     queryset = Tags.objects.all()
@@ -13,3 +13,6 @@ class TagsViewSet(viewsets.GenericViewSet, mixins.ListModelMixin):
 
     def get_queryset(self):
         return self.queryset.filter(user=self.request.user).order_by('-name')
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
