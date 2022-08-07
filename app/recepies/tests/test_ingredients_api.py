21a65cde9254deec1903728782afebcc3b5d9497
bakefayat <bakefayat98@gmail.com>
2022-08-07 11:08:10 +0430

    test creating ingredients added.

--- app/recepies/tests/test_ingredients_api.py
+++ app/recepies/tests/test_ingredients_api.py
@@ -61,3 +61,18 @@ class PrivateIngredientsApiTests(TestCase):
         self.assertEqual(res.status_code, status.HTTP_200_OK)
         self.assertEqual(len(res.data), 1)
         self.assertEqual(res.data[0]['name'], ingredient.name)
+
+    def test_create_ingredient_successful(self):
+        """Test that create a new ingredient is working properly"""
+        payload = {'name': 'banana', 'user': self.user}
+        res = self.client.post(INGREDIENTS_URL, payload)
+        exists = Ingredients.objects.filter(user=self.user, name=payload['name'])
+
+        self.assertTrue(exists)
+
+    def test_create_ingredient_invalid(self):
+        """Test creating a new ingredient with blank payload"""
+        payload = {'name': '',}
+        res = self.client.post(INGREDIENTS_URL, payload)
+
+        self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
